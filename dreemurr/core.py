6894d2b4ffc2689f8eb22367d4e1d6689a158130
isobel-p <173300585+isobel-p@users.noreply.github.com>
2026-07-29 16:18:53 +0100

    add core name generation feature

---
+++ dreemurr/core.py
@@ -0,0 +1,33 @@
+from dreemurr.utils import encode
+import os
+from openrouter import OpenRouter
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+DEFAULT_MODEL = "google/gemini-3.5-flash-lite"
+
+
+def generate(path:str, model:str=DEFAULT_MODEL) -> str:
+    image = encode(path)
+
+    client = OpenRouter(
+        api_key=API_KEY,
+        server_url="https://ai.hackclub.com/proxy/v1",
+    )
+
+    with open("prompt.txt") as f:
+        prompt = f.read()
+
+    response = client.chat.send(
+        model=model,
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": [
+                {"type": "text", "text": "Generate a filename for this image:"},
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}
+            ]}
+        ],
+    )
+    print(response.choices[0].message.content)
+    return response.choices[0].message.content
---
+++ dreemurr/utils.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import base64
+from io import BytesIO
+
+def encode(path, max_size=1024) -> str:
+    with Image.open(path) as img:
+        if img.mode in ("RGBA", "LA", "P"):
+            img = img.convert("RGB")
+
+    width, height = img.size 
+    if width > max_size or height > max_size:
+        ratio = min(max_size/width, max_size/height)
+        size = (int(width*ratio), int(height*ratio))
+        img = img.resize(size, Image.Resampling.LANCZOS)
+    
+    buffer = BytesIO()
+    img.save(buffer, format="JPEG")
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
