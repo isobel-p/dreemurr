1f5f7aa6fba429bed175bbfce330114e14b172d9
Isobel <173300585+isobel-p@users.noreply.github.com>
2026-07-30 13:42:14 +0100

    fix typo that was causing annoying bug :(

--- dreemurr/utils.py
+++ dreemurr/utils.py
@@ -7,12 +7,12 @@ def encode(path, max_size=1024) -> str:
         if img.mode in ("RGBA", "LA", "P"):
             img = img.convert("RGB")
 
-    width, height = img.size 
-    if width > max_size or height > max_size:
-        ratio = min(max_size/width, max_size/height)
-        size = (int(width*ratio), int(height*ratio))
-        img = img.resize(size, Image.Resampling.LANCZOS)
-    
-    buffer = BytesIO()
-    img.save(buffer, format="JPEG")
-    return base64.b64encode(buffer.getvalue()).decode("utf-8")
+        width, height = img.size 
+        if width > max_size or height > max_size:
+            ratio = min(max_size/width, max_size/height)
+            size = (int(width*ratio), int(height*ratio))
+            img = img.resize(size, Image.Resampling.LANCZOS)
+        
+        buffer = BytesIO()
+        img.save(buffer, format="JPEG")
+        return base64.b64encode(buffer.getvalue()).decode("utf-8")
